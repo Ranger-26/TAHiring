f78289b7761ec4307221990500e36dcb42df5d96
smohan67 <siddhu.mohan6@gmail.com>
2023-10-28 16:04:46 -0500

    addin some testers

--- src/HiringTesting.java
+++ src/HiringTesting.java
@@ -27,11 +27,13 @@ public class HiringTesting {
     expectedList.add(testingList.get(0));
     expectedList.add(testingList.get(1));
     CandidateList actual = Hiring.greedyHiring(testingList, new CandidateList(), 2);
+    CandidateList actual2 = Hiring.greedyHiring(new CandidateList(), new CandidateList(), 2);
+
     System.out.println("------------END----------------");
     printCandidateList(actual);
     printCandidateList(expectedList);
 
-    return HiringTestingUtilities.compareCandidateLists(expectedList,actual);
+    return HiringTestingUtilities.compareCandidateLists(expectedList,actual) || HiringTestingUtilities.compareCandidateLists(actual2, new CandidateList());
   }
 
 
@@ -45,27 +47,39 @@ public class HiringTesting {
   }
 
   public static boolean optimalHiringBaseTest(){
-    return false;
+    CandidateList testingList = HiringTestingUtilities.makeCandidateList(new boolean[][]
+        {
+            {true, false, false, true, true, true},
+            {true, true, false, false, true, false},
+            {false, false, true, true, false, true}
+        });
+    CandidateList expected = HiringTestingUtilities.generateRandomInput(6, 5);
+    CandidateList actual = Hiring.optimalHiring(testingList, expected, 0);
+    System.out.println("------------END----------------");
+    printCandidateList(actual);
+    printCandidateList(expected);
+    return HiringTestingUtilities.compareCandidateLists(expected,actual);
   }
+
   public static boolean optimalHiringRecursiveTest(){
     CandidateList testingList = HiringTestingUtilities.makeCandidateList(new boolean[][]
         {
             {true, false, false, true, true, true},
             {true, true, false, false, true, false},
-            {false, false, true, true, false, false},
-            {true, false, true, false, false, true},
-            {false, true, false, true, true, false},
+            {false, false, true, true, false, true}
         });
     printCandidateList(testingList);
     CandidateList expectedList = new CandidateList();
-    expectedList.add(testingList.get(3));
-    expectedList.add(testingList.get(4));
+    expectedList.add(testingList.get(0));
+    expectedList.add(testingList.get(1));
     CandidateList actual = Hiring.optimalHiring(testingList, new CandidateList(), 2);
+    CandidateList actual2 = Hiring.optimalHiring(new CandidateList(), new CandidateList(), 2);
+
     System.out.println("------------END----------------");
     printCandidateList(actual);
     printCandidateList(expectedList);
 
-    return HiringTestingUtilities.compareCandidateLists(expectedList,actual);
+    return HiringTestingUtilities.compareCandidateLists(expectedList,actual) || HiringTestingUtilities.compareCandidateLists(actual2, new CandidateList());
   }
 
   public static boolean optimalHiringFuzzTest(){
@@ -75,6 +89,7 @@ public class HiringTesting {
   public static void main(String[] args){
     if (!greedyHiringRecursiveTest()){
       System.out.println("FAIL greedyHiringRecursiveTest!");
+      return;
     }
     else{
       System.out.println("PASS greedyHiringRecursiveTest!");
@@ -82,16 +97,24 @@ public class HiringTesting {
 
     if (!greedyHiringBaseTest()){
       System.out.println("FAIL greedyHiringBaseTest!");
+      return;
     }
     else {
       System.out.println("PASS greedyHiringBaseTest!");
     }
 
     if (optimalHiringRecursiveTest()){
-      System.out.println("PASS optimalHiringRecursiveTest!");
-    }else{
       System.out.println("FAIL optimalHiringRecursiveTest!");
+      return;
+    }else{
+      System.out.println("PASS optimalHiringRecursiveTest!");
+    }
 
+    if (optimalHiringBaseTest()){
+      System.out.println("PASS optimalHiringBaseTest!");
+      return;
+    }else{
+      System.out.println("FAIL optimalHiringNaseTest!");
     }
   }
 
